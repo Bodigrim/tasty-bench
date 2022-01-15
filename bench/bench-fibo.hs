50df1232e49e2105dfcb67b57f6c4bbfd814524b
Oleg Grenrus <oleg.grenrus@iki.fi>
2022-01-15 12:01:22 +0200

    Add bench-fibo example as proper benchmark
    Also move sources to src/ so there are no accidental rebuilding.

---
+++ bench/bench-fibo.hs
@@ -0,0 +1,15 @@
+module Main (main) where
+
+import Test.Tasty.Bench
+
+fibo :: Int -> Integer
+fibo n = if n < 2 then toInteger n else fibo (n - 1) + fibo (n - 2)
+
+main :: IO ()
+main = defaultMain
+  [ bgroup "fibonacci numbers"
+    [ bench "fifth"     $ nf fibo  5
+    , bench "tenth"     $ nf fibo 10
+    , bench "twentieth" $ nf fibo 20
+    ]
+  ]
